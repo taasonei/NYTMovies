fa63fa31858461cadecc9cd0b37367364b8f6f2b
Anna <taasonei@gmail.com>
2022-09-12 03:16:01 +0300

    feat: update activity_movie_list layout. add movie_item layout

--- app/src/main/java/com/github/taasonei/nytmovies/presentation/ui/MovieListActivity.kt
+++ app/src/main/java/com/github/taasonei/nytmovies/presentation/ui/MovieListActivity.kt
@@ -3,10 +3,17 @@ package com.github.taasonei.nytmovies.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.taasonei.nytmovies.R
+import com.github.taasonei.nytmovies.databinding.ActivityMovieListBinding
 
 class MovieListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMovieListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMovieListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
